f52324d4a1f5cc6e3396c1c3ae27ec35bb88015a
Joe OpenSrc <joe-opensrc@protonmail.ch>
2026-03-04 12:37:01 +0100

    nhobs -- ooh err. NetHack OBS. v0.0.1
    Small script to "save scumm" a nethack installation.

---
+++ README.md
@@ -0,0 +1,9 @@
+### nhobs -- ooh err. NetHack OBS.
+
+Small script to "save scumm" a nethack installation.
+
+Creates little separate dungeons without chroots, etc. \
+Not perfect but useful enough for my purposes.
+
+It makes an assumption that nethack \
+is installed in the following location: /var/games/nethack
---
+++ nh-obs.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+nhuser="gorviblax"
+
+build=0 # enum
+run=0 # bool
+OPTIND=1 OPTARG="" OPTERR=0
+
+hdir_short="hackdir";
+
+while getopts 'bd:u:' flag
+do
+  case "${flag}" in
+    b)  build=2;;
+    d)  hdir_short="${OPTARG}";;
+    u)  nhuser="${OPTARG}";;
+  esac
+  shift $(( ${OPTIND} - 1 ))
+done
+
+hdir="$( realpath "${hdir_short}" )"
+
+function build(){
+  local hdir="${1}"
+  scr="read an uncursed scroll of create directory"
+  case ${build} in
+  1)
+    echo -e "\nIt's too dark to find the hackdir! \"${hdir}\"" >&2
+    read -p "${scr}? [Y/n]" ans
+    ans="${ans:-Y}"
+    if [[ ${ans} != "Y" ]]
+    then
+      echo "...you decide not to risk it." >&2
+      exit 1
+    fi;;
+  2) :;;
+  *) return 0;;
+  esac
+
+  echo "You ${scr}." >&2
+  hdir="$( realpath -m "${hdir}" )"
+
+  # make base64?
+  mkdir -p "${hdir}"/{bones,save,level,lock,trouble}
+  cp "$( realpath $( dirname ${0} ) )"/dot-nethackrc "${hdir}"/
+  cp /var/games/nethack/{nhdat,symbols,sysconf,license} "${hdir}"/
+
+  cd "${hdir}"
+  touch perm logfile xlogfile
+  echo "Now re-cast this script to enter the maze." >&2
+  run=1
+}
+
+if [[ ${build} -eq 0 && ! -d "${hdir}" ]]
+then
+  build=1
+fi
+
+export NETHACKOPTIONS=@"${hdir}/dot-nethackrc" \
+       NETHACKDIR="${hdir}" \
+       LEVELDIR="${hdir}/level" \
+       SAVEDIR="${hdir}/save" \
+       BONESDIR="${hdir}/bones" \
+       LOCKDIR="${hdir}/lock" \
+       TROUBLEDIR="${hdir}/trouble"
+
+if [[ ${build} -gt 0 ]]
+then
+  build "${hdir_short}"
+fi
+
+if [[ ${run} -eq 0 ]]
+then
+  nethack -u "${nhuser}" "${@}"
+fi
